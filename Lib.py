ec034fb07ef4cd773f1072cc256e18d61b93f887
Ipsita Pal <136154134+Ipsy05@users.noreply.github.com>
2024-03-19 12:50:33 +0530

    Add py

---
+++ Lib.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#import the warnings.# Importing necessary libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Setting matplotlib to display plots inline
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+# Suppressing warnings
+import warnings
+warnings.filterwarnings('ignore')
+
